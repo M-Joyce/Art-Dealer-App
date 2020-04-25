d7c148c5f50ce6a80c7782025ed80edc4ffe4757
blank <vongk7@gmail.com>
2020-04-25 00:19:15 -0500

    Four of a kind, and two pairs added

--- js/cards.js
+++ js/cards.js
@@ -70,10 +70,23 @@ function cardNumberLookup(number, cardChoices){
     return null;
 }
 
+// Find a card in cardList
+function cardInCardList(card, cardList){
+    for (let i = 0; i < cardList.length; i++){
+        if (card.number  == cardList[i].number,
+            card.color == cardList[i].color,   
+            card.suit  == cardList[i].suit ) {
+            return true;    
+        }
+    }
+    return false;
+}
+
 // Function to match pattern with choices
-function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9], cards[3]]){
+function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9], cards[12]]){
     switch(patternNumber){
         case 0:
+            {
             // Check for straight
 
             // Get card number and sort them
@@ -87,8 +100,6 @@ function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9
             // Look for consecutive cards
             let consecutiveCards = [[]];
             let index = 0;
-            console.log('Sorted Cards:');
-            console.log(sortedCards);
             let currentCard = sortedCards[0];
             let nextCard = sortedCards[0];
             for (let i = 0; i < sortedCards.length -1; i++){
@@ -111,7 +122,6 @@ function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9
 
             // Account for ace as high card
             if (nextCard == 13) {
-                console.log('next is 13');
                 currentCard = 13
                 for (let i = 0; i < sortedCards.length; i++) {
                     if (sortedCards[i] == 1) {
@@ -131,7 +141,6 @@ function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9
                 }
             }
 
-            console.log(consecutiveCards);
             // Look for longest consecutive runs
             let maxConsecutive = 0;
             for (let i = 0; i < consecutiveCards.length; i++){
@@ -140,16 +149,14 @@ function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9
                     index = i;
                 }
             }
-            console.log(consecutiveCards[index]);
 
             // Get return cards
-            returnCards = [];
+            let returnCards = [];
             for ( let i = 0; i < consecutiveCards[index].length; i++){
                 let returnCard = cardNumberLookup(
                     consecutiveCards[index][i], cardChoices);
                 returnCards.push(returnCard);
             }
-            console.log(returnCards);
 
             // Return null if no consecutive cards
             if (returnCards.length == 0)
@@ -157,18 +164,170 @@ function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9
             else
                 return returnCards;
             break;
+            }
         case 1:
+            {
+            // Primes
+            console.log(cardChoices);
+            let returnCards = [];
+            for (let i = 0; i < cardChoices.length; i++) {
+                number = cardChoices[i].number;
+                if (number == 2 || number == 3 || number == 5 || 
+                    number == 7 || number == 11 || number == 13) {
+                    returnCards.push(cardChoices[i]);
+                }
+            }
+            if (returnCards.length == 0)
+                return null;
+            else
+                return returnCards;
             console.log('Pattern number is 1');
             break;
+            }
         case 2:
+            {
+            // Evens
             console.log('Pattern number is 2');
+            console.log(cardChoices);
+            let returnCards = [];
+            for (let i = 0; i < cardChoices.length; i++) {
+                number = cardChoices[i].number;
+                if (number % 2 == 0) {
+                    returnCards.push(cardChoices[i]);
+                }
+            }
+            if (returnCards.length == 0)
+                return null;
+            else
+                return returnCards;
             break;
+            }
         case 3:
+            {
+            // Odds
             console.log('Pattern number is 3');
+            console.log(cardChoices);
+            let returnCards = [];
+            for (let i = 0; i < cardChoices.length; i++) {
+                let number = cardChoices[i].number;
+                if (number % 2 != 0) {
+                    returnCards.push(cardChoices[i]);
+                }
+            }
+            if (returnCards.length == 0)
+                return null;
+            else
+                return returnCards;
             break;
+            }
         case 4:
+            {
+            // Four of a kind
             console.log('Pattern number is 4');
+            console.log(cardChoices);
+            let returnCards = [];
+            let sortedCards = [];
+            for (let i = 0; i < cardChoices.length; i++){
+                sortedCards.push(Number(cardChoices[i].number));
+            }
+            // Sort in ascending order
+            sortedCards = sortedCards.sort(function(a, b){return a-b});
+            console.log(sortedCards);
+
+            // If first and last index of sorted cards are the same number then we have four of a kind.
+            if (sortedCards[0] == sortedCards[sortedCards.length - 1]) {
+                return cardChoices;
+            }
+
+            let cardList = []
+
+            // Return identical numbers
+            for (let i = 0; i < cardChoices.length; i++){
+                currentCard = cardChoices[i];
+                for (let j = 0; j < cardChoices.length; j++){
+                    if (i != j){
+                        if (currentCard.number == cardChoices[j].number) {
+                            if (cardList.length == 0) {
+                                cardList.push(currentCard);
+                                cardList.push(cardChoices[j]);
+                                currentCard = cardChoices[j];
+                            }
+                            else{
+                                cardList.push(cardChoices[j]);
+                                currentCard = cardChoices[j];
+                            }
+                        }
+                    }
+                }
+            }
+            console.log('matches');
+            console.log(cardList);
+
+
+            // Removing duplicates from cardList
+            if (cardList.length > 0) {
+                nonDuplicates = [];
+                mark = [];
+                for (let i = 0; i < cardList.length; i++){
+                    mark.push(true);
+                }
+                for (let i = 0; i < cardList.length; i++){
+                    if (mark[i] == true) {
+                        for (let j = i + 1; j < cardList.length; j++){
+                            if (cardList[j].index == cardList[i].index) {
+                                mark[j] = false;
+                            }
+                        }
+                    }
+                }       
+                for (let i = 0; i < cardList.length; i++){
+                    if (mark[i] == true) {
+                        nonDuplicates.push(cardList[i]);
+                    }
+                }
+                console.log('Nondups');
+                return nonDuplicates;
+            }
+            else
+                return null;
             break;
+            }
+        case 5:
+            {
+            // Two Pairs
+            console.log('Pattern number 5');
+            console.log(cardChoices);
+            let returnCards = [];
+            let sortedCards = [];
+            for (let i = 0; i < cardChoices.length; i++){
+                sortedCards.push(Number(cardChoices[i].number));
+            }
+            // Sort in ascending order
+            sortedCards = sortedCards.sort(function(a, b){return a-b});
+            console.log(sortedCards);
+
+            if (sortedCards[0] == sortedCards[1] && sortedCards[2] == sortedCards[3]) {
+                return cardChoices;
+            }
+
+            // Get all two pairs
+            for (let i = 0; i < cardChoices.length; i++){
+                for (let j = i + 1; j < cardChoices.length; j++){
+                    if (cardChoices[i].number == cardChoices[j].number) {
+                        if (returnCards.length == 0) {
+                            returnCards.push(cardChoices[i]);
+                            returnCards.push(cardChoices[j]);
+                        }
+                    }
+                }
+            }
+
+            if (returnCards.length == 0)
+                return null;
+            else
+                return returnCards;
+            break;
+            }
         default:
             console.log('cards.js: Invalid pattern number.');
             console.log(patternNumber)
@@ -176,5 +335,5 @@ function matchPatterns(patternNumber, cardChoices = [cards[1], cards[5], cards[9
     
 }
 
-returnedCards = matchPatterns(0)
+returnedCards = matchPatterns(5, [cards[1], cards[4], cards[15], cards[17]])
 console.log(returnedCards);
