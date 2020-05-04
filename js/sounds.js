b886b1bdff59ffde7f9cfac442e25b4c0cc3809f
Mark4751 <31703844+Mark4751@users.noreply.github.com>
2020-05-04 12:08:10 -0500

    Update sounds.js
    reduced background music volume to better hear instructions.

--- js/sounds.js
+++ js/sounds.js
@@ -7,7 +7,7 @@
 // Play background automatically on load
 var backgroundAudio = document.getElementById("backgroundAudio");
 backgroundAudio.loop = true;
-backgroundAudio.volume = 1;
+backgroundAudio.volume = 0.75;
 backgroundAudio.play();
 
 // Mute function to mute any outgoing volume
@@ -20,7 +20,7 @@ function mute() {
         // Add any other sound to mute here
     } else {
         muteButton.innerHTML = "Mute";
-        backgroundAudio.volume = 1; 
+        backgroundAudio.volume = 0.75; 
         // Add any other sound to unmute here
     }
 } 
